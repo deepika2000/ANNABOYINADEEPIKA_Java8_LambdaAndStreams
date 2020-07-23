8636e7b3e83b691f346c6e885d7a89ad45a308d1
Deepika <17wh1a1258@bvrithyderabad.edu.in>
2020-07-23 20:14:38 +0530

    files added

--- EPAM_SimpleInterest/src/main/java/com/epam/EPAM_SimpleInterest/CompoundInterest.java
+++
@@ -1,8 +0,0 @@
-package com.epam.EPAM_SimpleInterest;
-
-public class CompoundInterest {
-	public double compoundinterest(double principle_amount, double rate, double time) {
-		double ci=principle_amount * Math.pow(1.0+rate/100.0,time) - principle_amount;
-		return ci;
-	}
-}
